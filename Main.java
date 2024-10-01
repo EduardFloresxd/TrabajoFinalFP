3133970ee6753cf148bd92e61d401bd84dea70e6
EduardFloresxd <57373048+EduardFloresxd@users.noreply.github.com>
2024-10-01 18:09:18 -0500

    PROJECTV2
    Se añaden arreglos al programa para almacenar la información de múltiples trabajadores de forma mas organizada y eficiente.

--- Main.java
+++ Main.java
@@ -5,41 +5,55 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String trabajador;
-        double horasTrabajadas, horasPerdidas, horasExtra, bonos;
+        String[] trabajador = new String[10]; // Arreglo para recolectar la informacion de 10 trabajadores
+        double[] horasTrabajadas = new double[10];
+        double[] horasPerdidas = new double[10];
+        double[] horasExtra = new double[10];
+        double[] bonos = new double[10];
 
         char continuar = 's';
 
-        //Usaremos el while para que el algoritmo corra cada vez que solicitemos nueva informacion de un trabajador.
         while (continuar == 's' || continuar == 'S') {
-            System.out.println("Ingrese el nombre del trabajador:");
-            trabajador = scanner.nextLine();
 
-            System.out.println("Ingrese las horas trabajadas:");
-            horasTrabajadas = scanner.nextDouble();
+            for (int i = 0; i < trabajador.length; i++) {
+                System.out.println("Ingrese el nombre del trabajador " + (i+1) + ":");
+                trabajador[i] = scanner.nextLine();
 
-            System.out.println("Ingrese las horas trabajadas perdidas:");
-            horasPerdidas = scanner.nextDouble();
+                System.out.println("Ingrese las horas trabajadas:");
+                horasTrabajadas[i] = scanner.nextDouble();
 
-            System.out.println("Ingrese las horas extra trabajadas:");
-            horasExtra = scanner.nextDouble();
+                System.out.println("Ingrese las horas trabajadas perdidas:");
+                horasPerdidas[i] = scanner.nextDouble();
 
-            System.out.println("Ingrese los ingresos por bonos:");
-            bonos = scanner.nextDouble();
+                System.out.println("Ingrese las horas extra trabajadas:");
+                horasExtra[i] = scanner.nextDouble();
 
-            // Aqui se calcula el valor final del sueldo en base a calculos matematicos
-            double sueldoBase = horasTrabajadas * 7; // Suponiendo 7 soles por hora
-            double descuentos = horasPerdidas * 7; // Suponiendo 7 soles de descuento por hora perdida, el precio es igual
-            double sueldoTotal = sueldoBase + horasExtra * 10 + bonos - descuentos; //Las horas extra tienen un aumento, lo que hace que paguen 10 soles
+                System.out.println("Ingrese los ingresos por bonos:");
+                bonos[i] = scanner.nextDouble();
 
-            System.out.println("Boleta de " + trabajador);
-            System.out.println("Horas trabajadas: " + horasTrabajadas);
-            System.out.println("Horas perdidas: " + horasPerdidas);
-            System.out.println("Horas extra: " + horasExtra);
-            System.out.println("Bonos: S/" + bonos +" nuevos soles.");
-            System.out.println("Sueldo total: S/ " + sueldoTotal+" nuevos soles.");
+            }
 
-            System.out.println("\n¿Desea ingresar otro trabajador? (s/n):");
+            for (int i = 0; i < trabajador.length; i++) {
+
+                double sueldoBase = horasTrabajadas[i] * 7; // Suponiendo 7 soles por hora
+                double descuentos = horasPerdidas[i] * 7; // Suponiendo 7 soles de descuento por hora perdida, el precio es igual
+                double pagoHorasExtra = horasExtra[i] * 10; // Calculamos cuanto le deben pagar por hora extra
+                double sueldoTotal = sueldoBase + pagoHorasExtra + bonos[i] - descuentos; //Las horas extra tienen un aumento, lo que hace que paguen 10 soles
+
+                System.out.println("-----------------------------------------------");
+                System.out.println("Boleta de " + trabajador[i]);
+                System.out.println("Horas trabajadas: " + horasTrabajadas[i]);
+                System.out.println("Horas perdidas: " + horasPerdidas[i]);
+                System.out.println("Horas extra: " + horasExtra[i]);
+                System.out.println("Bonos: S/" + bonos[i] +" nuevos soles.");
+                System.out.println("Sueldo total: S/ " + sueldoTotal+" nuevos soles.");
+                System.out.println("-----------------------------------------------");
+                System.out.println(" ");
+
+
+            }
+
+            System.out.println("\n¿Desea calcular las boletas de otros 10 trabajadores? (s/n):");
             continuar = scanner.next().charAt(0);
         }
 
